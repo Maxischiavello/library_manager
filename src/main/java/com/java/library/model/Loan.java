2daaa59a42f9197a65ad706ce5a3ceaa74f0ac06
maxischiavello <maxischiavello12@gmail.com>
2024-07-18 10:22:55 -0300

    first commit - library manager backend

---
+++ src/main/java/com/java/library/model/Loan.java
@@ -0,0 +1,25 @@
+package com.java.library.model;
+
+import lombok.Data;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+}
---
+++ src/main/java/com/java/library/model/User.java
@@ -0,0 +1,17 @@
+package com.java.library.model;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+}
